0fcac5e8ccc84af175d65e2b7a4d77815388c62f
Nattapon <jammy51000@gmail.com>
2024-01-13 01:07:52 +0700

    Update Unit Testing, Documentation, and Object-Oriented Programming

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using NUnit.Framework;
+
 class Program
 {
     static void Main()
@@ -7,24 +9,81 @@ class Program
         
         Console.Write("Input: ");
         string userInput = Console.ReadLine()!;
+        Boolean CheckInputLoop = true;
+        // The program will run when the correct values are entered.
+        while (CheckInputLoop)
+        {
+            if (CheckInput(userInput))
+            {
+                Oldphonekeypad inputOld = new Oldphonekeypad(userInput);
+                
+                // Console.WriteLine($"Output: {inputOld.DisplayOldphonekeypad()}");
+                Console.WriteLine("Output: " + inputOld.DisplayOldphonekeypad());
+                CheckInputLoop = false;
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.Write("The entered value is invalid. Please enter a new value: ");
+                userInput = Console.ReadLine()!;
+            }
+        }
 
-        string pad = OldPhonePad(userInput);
-        Console.WriteLine($"Output: {pad}");
+
+        // test
+        // PhoneTest t = new PhoneTest();
+        // t.DisplayOldphonekeypad("33#", "E");
+        // t.DisplayOldphonekeypad("227*#", "B");
+        // t.DisplayOldphonekeypad("4433555 555666#", "HELLO");
+        // Console.WriteLine("All ok!!!..");
         
-
+    }
+    // The function checks each value to see if there are any invalid values in the string. 
+    static Boolean CheckInput(string input)
+    {
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c) || c == '\b')
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    static string OldPhonePad(String input) 
-    {
-        input = input.Substring(0, input.Length-1);
-        string text = "";
+    
+}   
 
+// class Oldphonekeypad
+public class Oldphonekeypad
+{
+    // Properties
+    private string Input { get; set; } 
+
+    // Constructor
+    public Oldphonekeypad(string input)
+    {
+        Input = input;
+        
+    }
+
+    // Method
+    public string DisplayOldphonekeypad()
+    {
+        // Delete the last character that is *
+        Input = Input.Substring(0, Input.Length-1);
+        // text is output
+        string text = "";
+        // recount is counting the number of repeated characters.
         int recount = 0;
+        // prechar is prechar is the previous character. 
+        // When new characters are found, the previous characters are added to the text.
         char prechar = ' ';
-        int lengthinput = input.Length;
+        // When lengthinput is equal to lengthinputcheck The current character is added to the text.
+        int lengthinput = Input.Length;
         int lengthinputcheck = 0;
         
-        
+        // The dictionary uses [prechar] as the key, and [recount] as the position in the list.
         Dictionary<char, List<char>> numData = new Dictionary<char, List<char>>();
 
         numData['0'] = new List<char> { ' ' };
@@ -38,15 +97,17 @@ class Program
         numData['8'] = new List<char> { 'T', 'U', 'V' };
         numData['9'] = new List<char> { 'W', 'X', 'Y', 'Z' };
 
-        foreach (char c in input)
+        foreach (char c in Input)
         {   
             lengthinputcheck += 1;
+            // When starting or encountering a new char, recount is 0.
             if (recount == 0 )
             {   
                 // protect start with * --> *33#
                 if (c != '*' && c != ' ')
                 {
-                    
+                    // preIf prechar is equal to 'c', when a new value is encountered, 
+                    // add prechar to the text and update prechar to the new value encountered.
                     prechar = c;
                     recount += 1;
                 }
@@ -56,35 +117,41 @@ class Program
             else if ( c == '*' )
             {   
                 //  2222#
+                // Add prechar to text and recount to 0.
                 text = Loopnum(recount, numData, prechar, text);
+                //  Delete the last character from text.
                 text = text.Substring(0, text.Length-1);
                 recount = 0;   
             }
             else if ( c == ' ')
             {
                 //  2222#
+                // Add prechar to text and recount to 0.
                 text = Loopnum(recount, numData, prechar, text);
                 recount = 0;   
                 
             }
+
             else
             {
-                // Repeat the previous letter
+                // Encountering repeated characters.
                 if ( prechar == c )
                 {
                     recount += 1;
                 }
-                // New number
+                // Encountering a new character.
                 else
                 {   
                     //  2222#
+                    // Add prechar to text and recount to 0.
                     text = Loopnum(recount, numData, prechar, text);
+                    // recount = 1  The encountered character is the first one.
                     recount = 1;
                     prechar = c;
                     
                 }
             }
-            // put last char --> 333 #, 3#,  33  33   33   #
+            // Add the character encountered at the end to the text. --> 333 #, 3#,  33  33   33   #
             if ( lengthinput == lengthinputcheck && c != '*' && c != ' ')
             {   
                 //  2222#
@@ -93,12 +160,14 @@ class Program
             }
         }
         
-
-
+        //  return output
         return text;
+        // Console.WriteLine($"Output: {text}");
     }
 
-    static string Loopnum(int recount, Dictionary<char, List<char>> numData, char prechar, string text) 
+    // The function Loopnum checks, when encountering a duplicate character greater than what is in the dictionary, 
+    // it performs a mod operation to find the value.
+    private string Loopnum(int recount, Dictionary<char, List<char>> numData, char prechar, string text) 
     {
         //  check if 33333#, 1111122222#
         if (recount-1 >= numData[prechar].Count)
@@ -112,3 +181,26 @@ class Program
         return text;
     }
 }
+// Test
+[TestFixture]
+public class PhoneTest
+{
+    //      output : input
+    // [TestCase("33#", "E")]
+    // [TestCase("227*#", "B")]
+    // [TestCase("555", "L")]
+    public void DisplayOldphonekeypad(string input, string expectedResult)
+    {
+        // Arrange
+        Oldphonekeypad oldphonekeypad = new Oldphonekeypad(input);
+
+        // Act
+        string result = oldphonekeypad.DisplayOldphonekeypad();
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+        Console.WriteLine("All ok!!!");
+    }
+
+}
+
